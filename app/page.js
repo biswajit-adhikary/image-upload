0e7da4b1e0ba8ae61532f665b55adbd1e95035cd
Biswajit Adhikary <biswajitadhikary.web@gmail.com>
2025-01-27 01:22:25 +0600

    Image Upload Ready

--- app/page.js
+++ app/page.js
@@ -3,8 +3,6 @@ import FileUpload from "@/components/FileUpload";
 export default function Home() {
   return (
     <>
-      <h1>Image Upload</h1>
-      <h2>Upload Image</h2>
       <FileUpload />
     </>
   );
